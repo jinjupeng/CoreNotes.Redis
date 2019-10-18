4948e6106658b51f76eb17030079ee873f04c882
jinjupeng <im.jp@outlook.com>
2019-10-18 20:20:48 +0800

    添加项目文件。

---
+++ CoreNotes.Redis/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CoreNotes.Redis.Models;
+using StackExchange.Redis;
+
+namespace CoreNotes.Redis.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConnectionMultiplexer _redis;
+        private readonly ILogger<HomeController> _logger;
+        private readonly IDatabase _db;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public HomeController(IConnectionMultiplexer redis)
+        {
+            _redis = redis;
+            _db = _redis.GetDatabase();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
